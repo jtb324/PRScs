a7ebd02640ff9d6dfcdd00a244c9524c3664f2b6
jtb <james.baker@vanderbilt.edu>
2023-09-28 15:57:32 -0500

    converted all of the python code to python 3 using 2to3 and then corrected some of the linting error. Code now works with python 3.11

--- gigrnd.py
+++ gigrnd.py
@@ -13,12 +13,12 @@ from numpy import random
 
 
 def psi(x, alpha, lam):
-    f = -alpha*(math.cosh(x)-1.0)-lam*(math.exp(x)-x-1.0)
+    f = -alpha * (math.cosh(x) - 1.0) - lam * (math.exp(x) - x - 1.0)
     return f
 
 
 def dpsi(x, alpha, lam):
-    f = -alpha*math.sinh(x)-lam*(math.exp(x)-1.0)
+    f = -alpha * math.sinh(x) - lam * (math.exp(x) - 1.0)
     return f
 
 
@@ -35,9 +35,11 @@ def g(x, sd, td, f1, f2):
 
 def gigrnd(p, a, b):
     # setup -- sample from the two-parameter version gig(lam,omega)
-    p = float(p); a = float(a); b = float(b)
+    p = float(p)
+    a = float(a)
+    b = float(b)
     lam = p
-    omega = math.sqrt(a*b)
+    omega = math.sqrt(a * b)
 
     if lam < 0:
         lam = -lam
@@ -45,7 +47,7 @@ def gigrnd(p, a, b):
     else:
         swap = False
 
-    alpha = math.sqrt(math.pow(omega,2)+math.pow(lam,2))-lam
+    alpha = math.sqrt(math.pow(omega, 2) + math.pow(lam, 2)) - lam
 
     # find t
     x = -psi(1.0, alpha, lam)
@@ -55,12 +57,12 @@ def gigrnd(p, a, b):
         if (alpha == 0) and (lam == 0):
             t = 1.0
         else:
-            t = math.sqrt(2.0/(alpha+lam))
+            t = math.sqrt(2.0 / (alpha + lam))
     elif x < 0.5:
         if (alpha == 0) and (lam == 0):
             t = 1.0
         else:
-            t = math.log(4.0/(alpha+2.0*lam))
+            t = math.log(4.0 / (alpha + 2.0 * lam))
 
     # find s
     x = -psi(-1.0, alpha, lam)
@@ -70,16 +72,25 @@ def gigrnd(p, a, b):
         if (alpha == 0) and (lam == 0):
             s = 1.0
         else:
-            s = math.sqrt(4.0/(alpha*math.cosh(1)+lam))
+            s = math.sqrt(4.0 / (alpha * math.cosh(1) + lam))
     elif x < 0.5:
         if (alpha == 0) and (lam == 0):
             s = 1.0
         elif alpha == 0:
-            s = 1.0/lam
+            s = 1.0 / lam
         elif lam == 0:
-            s = math.log(1.0+1.0/alpha+math.sqrt(1.0/math.pow(alpha,2)+2.0/alpha))
+            s = math.log(
+                1.0 + 1.0 / alpha + math.sqrt(1.0 / math.pow(alpha, 2) + 2.0 / alpha)
+            )
         else:
-            s = min(1.0/lam, math.log(1.0+1.0/alpha+math.sqrt(1.0/math.pow(alpha,2)+2.0/alpha)))
+            s = min(
+                1.0 / lam,
+                math.log(
+                    1.0
+                    + 1.0 / alpha
+                    + math.sqrt(1.0 / math.pow(alpha, 2) + 2.0 / alpha)
+                ),
+            )
 
     # find auxiliary parameters
     eta = -psi(t, alpha, lam)
@@ -87,36 +98,36 @@ def gigrnd(p, a, b):
     theta = -psi(-s, alpha, lam)
     xi = dpsi(-s, alpha, lam)
 
-    p = 1.0/xi
-    r = 1.0/zeta
+    p = 1.0 / xi
+    r = 1.0 / zeta
 
-    td = t-r*eta
-    sd = s-p*theta
-    q = td+sd
+    td = t - r * eta
+    sd = s - p * theta
+    q = td + sd
 
     # random variate generation
     while True:
         U = random.random()
         V = random.random()
         W = random.random()
-        if U < q/(p+q+r):
-            rnd = -sd+q*V
-        elif U < (q+r)/(p+q+r):
-            rnd = td-r*math.log(V)
+        if U < q / (p + q + r):
+            rnd = -sd + q * V
+        elif U < (q + r) / (p + q + r):
+            rnd = td - r * math.log(V)
         else:
-            rnd = -sd+p*math.log(V)
+            rnd = -sd + p * math.log(V)
 
-        f1 = math.exp(-eta-zeta*(rnd-t))
-        f2 = math.exp(-theta+xi*(rnd+s))
-        if W*g(rnd, sd, td, f1, f2) <= math.exp(psi(rnd, alpha, lam)):
+        f1 = math.exp(-eta - zeta * (rnd - t))
+        f2 = math.exp(-theta + xi * (rnd + s))
+        if W * g(rnd, sd, td, f1, f2) <= math.exp(psi(rnd, alpha, lam)):
             break
 
     # transform back to the three-parameter version gig(p,a,b)
-    rnd = math.exp(rnd)*(lam/omega+math.sqrt(1.0+math.pow(lam,2)/math.pow(omega,2)))
+    rnd = math.exp(rnd) * (
+        lam / omega + math.sqrt(1.0 + math.pow(lam, 2) / math.pow(omega, 2))
+    )
     if swap:
-        rnd = 1.0/rnd
+        rnd = 1.0 / rnd
 
-    rnd = rnd/math.sqrt(a/b)
+    rnd = rnd / math.sqrt(a / b)
     return rnd
-
-
